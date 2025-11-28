bf53de7bcdf7ae73a15d582b29c905c4abbdf463
insiya25 <mca24.rizvi.insiya@gnims.com>
2025-11-28 18:21:45 +0530

    increased access token lifetime to 40 mins

--- attendance_backend/settings.py
+++ attendance_backend/settings.py
@@ -159,7 +159,7 @@ CORS_ALLOWED_ORIGINS = [
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=40),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
 
